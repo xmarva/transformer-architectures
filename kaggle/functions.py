7012cb81562d60bebf18f26510cae4a0773e6604
xmarva <elizaveta.victoria.koroleva@gmail.com>
2025-03-31 20:22:58 -0300

    feat: add translation dataset class

--- kaggle/functions.py
+++ kaggle/functions.py
@@ -2,6 +2,24 @@ import numpy as np
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
+class TranslationDataset(Dataset):
+    def __init__(self, processed_data):
+        self.input_ids = processed_data['input_ids']
+        self.attention_mask = processed_data['attention_mask']
+        self.labels = processed_data['labels']
+        self.decoder_attention_mask = processed_data['decoder_attention_mask']
+        
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return {
+            'input_ids': torch.tensor(self.input_ids[idx], dtype=torch.long),
+            'attention_mask': torch.tensor(self.attention_mask[idx], dtype=torch.long),
+            'labels': torch.tensor(self.labels[idx], dtype=torch.long),
+            'decoder_attention_mask': torch.tensor(self.decoder_attention_mask[idx], dtype=torch.long)
+        }
+
 def load_translation_dataset():
     print("Loading Tatoeba en-ru...")
     try:
