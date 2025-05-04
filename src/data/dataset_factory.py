bd283348eb06cdb6d797fb88ba70c76beb52c169
xmarva <elizaveta.victoria.koroleva@gmail.com>
2025-05-04 18:49:51 -0300

    fix(dataset_factory): set @lru_cache back :)

--- src/data/dataset_factory.py
+++ src/data/dataset_factory.py
@@ -4,17 +4,19 @@ from datasets import Dataset, load_dataset
 
 class DatasetFactory:
     @staticmethod
+    @lru_cache(maxsize=10)
     def load_dataset(name: str, **kwargs) -> Dataset:
-        kwargs["trust_remote_code"] = True
         try:
+            if "trust_remote_code" not in kwargs:
+                kwargs["trust_remote_code"] = True
             dataset = load_dataset(name, **kwargs)
             return dataset
         except Exception as e:
             raise ValueError(f"Failed to load dataset {name}: {str(e)}")
     
     @classmethod
-    def load_translation_dataset(cls, name: str = "Helsinki-NLP/tatoeba", trust_remote_code=True, lang1: str = "en", lang2: str = "ru") -> Dict:
-        dataset = cls.load_dataset(name, trust_remote_code=True, lang1=lang1, lang2=lang2)
+    def load_translation_dataset(cls, name: str = "Helsinki-NLP/tatoeba", lang1: str = "en", lang2: str = "ru") -> Dict:
+        dataset = cls.load_dataset(name, lang1=lang1, lang2=lang2, trust_remote_code=True)
         return dataset
     
     @staticmethod
